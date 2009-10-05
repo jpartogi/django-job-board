b3a4bd9ead71db320aff45797528fa7c10831a0f
Joshua Partogi <joshua.java@gmail.com>
2009-10-05 06:24:21 +0000

    added job category to admin

--- job_board/admin.py
+++ job_board/admin.py
@@ -10,11 +10,12 @@ class JobCategoryAdmin(admin.ModelAdmin):
 class JobAdmin(admin.ModelAdmin):
     date_hierarchy = 'posted'
     search_fields = ['title', 'description']
-    list_display = ['title', 'job_type', 'skills_required', 'location', 'company_name', 'onsite_required', 'posted']
-    list_filter = ['posted', 'job_type', 'location', 'onsite_required']
+    list_display = ['title', 'job_type', 'category', 'skills_required',
+                    'location', 'company_name', 'onsite_required', 'posted']
+    list_filter = ['posted', 'job_type', 'category', 'onsite_required']
     fieldsets = (
         ('Job Information', {
-            'fields': ('title', 'slug', 'job_type', 'description', 'skills_required', ('location', 'onsite_required')),
+            'fields': ('title', 'slug', 'job_type', 'category', 'description', 'skills_required', ('location', 'onsite_required')),
         }),
         ('Company Information', {
             'fields': ('company_name', ('to_apply'), 'website'),
