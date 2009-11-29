8411a2845cabfd3a21e68df74b2e4264a8c536a5
Joshua Partogi <jpartogi@vostro.(none)>
2009-11-29 14:04:57 +1100

    added assertion in test case and json type fixture

--- job_board/tests.py
+++ job_board/tests.py
@@ -3,9 +3,13 @@ from django.test import TestCase
 from job_board.models import Job
 
 class JobTestCase(TestCase):
-    fixtures = ['jobs']
+    fixtures = ['jobs.json']
     
     def testDisplay30DaysOldJob(self):
+        jobs = Job.objects.all()
+        self.assert_(len(jobs) == 1)
+
         jobs = Job.objects.filter()
         # Should not get any job because it's backdate is more than 30 days old
         self.assert_(len(jobs) == 0)
+
